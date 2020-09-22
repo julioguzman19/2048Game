533a50b8d0d5be915d47373f96a07f3b8899bfe6
Julio <julioguzman19@gmail.com>
2020-09-22 12:57:55 -0600

    Adding functions cpp and hpp files

---
+++ src/functions.cpp
@@ -0,0 +1,8 @@
+//
+//  functions.cpp
+//  testSFML
+//
+//  Created by Julio guzman on 9/22/20.
+//
+
+#include "functions.hpp"
---
+++ src/functions.hpp
@@ -0,0 +1,13 @@
+//
+//  functions.hpp
+//  testSFML
+//
+//  Created by Julio guzman on 9/22/20.
+//
+
+#ifndef functions_hpp
+#define functions_hpp
+
+#include <stdio.h>
+
+#endif /* functions_hpp */
