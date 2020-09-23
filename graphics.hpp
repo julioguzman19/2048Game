845b6ee939dc3e9def5669ff8f5a9e8865524307
rexcolin <rex.colin.mitchell@gmail.com>
2020-09-23 10:07:44 -0600

    graphics files, the code is commented out

---
+++ graphics.hpp
@@ -0,0 +1,13 @@
+//
+//  graphics.hpp
+//  testSFML
+//
+//  Created by Rex Colin Mitchell on 9/23/20.
+//
+
+#ifndef graphics_hpp
+#define graphics_hpp
+
+#include <stdio.h>
+
+#endif /* graphics_hpp */
